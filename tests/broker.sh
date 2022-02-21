149b42592f0c9f35a3614ff9125caceda6c98b12
Peter McGarrigle <peter.mcgarrigle@bt.com>
2022-02-21 08:55:25 +0000

    persisting zookeeper data

--- tests/broker.sh
+++ tests/broker.sh
@@ -9,25 +9,11 @@ kafka-topics.sh --bootstrap-server ${BROKER} --topic ${TOPIC} --describe
 
 echo
 echo '#publish'
-echo "///// message $(date) //" |kafka-console-producer.sh --bootstrap-server kafka:29092 --topic ${TOPIC} 
+echo "//// message $(date) //" |kafka-console-producer.sh --bootstrap-server kafka:29092 --topic ${TOPIC} 
 echo '#publish complete'
 
 # export KAFKA_OPTS="-Dlog4j.configuration=file:${HERE}/log4j.properties"
 echo
 echo '#subscribe'
-kafka-console-consumer.sh --bootstrap-server ${BROKER} --topic ${TOPIC} --from-beginning --timeout-ms 10000
-echo '#subscribe complete'
-
-exit
-
-echo '#list'
-kafkacat -b $BROKER -L
-echo '#list complete'
-
-echo '#publish'
-date | kafkacat -v -v -v -v -P -b $BROKER -t x
-echo '#publish complete'
-
-echo '#subscribe'
-kafkacat -C -e -b $BROKER -t x
+kafka-console-consumer.sh --bootstrap-server ${BROKER} --topic ${TOPIC} --from-beginning --timeout-ms 5000
 echo '#subscribe complete'
